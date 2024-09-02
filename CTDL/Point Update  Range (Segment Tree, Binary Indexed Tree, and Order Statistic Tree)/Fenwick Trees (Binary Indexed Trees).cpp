15f100fe8590513a780da8c5a6e966044e44c02b
unknown <dinhtuananhhoang@gmail.com>
2024-09-02 17:30:42 +0700

    update BIT

--- CTDL/Point Update  Range (Segment Tree, Binary Indexed Tree, and Order Statistic Tree)/Fenwick Trees (Binary Indexed Trees).cpp
+++ CTDL/Point Update  Range (Segment Tree, Binary Indexed Tree, and Order Statistic Tree)/Fenwick Trees (Binary Indexed Trees).cpp
@@ -1,9 +1,11 @@
 /*
  Vi du: prefix sum nhung can update va goi ra trong log(n)
- bit.set(n); giong bit.push_back()
+ nho khai bao BIT<int> bit
+ bit.set(i,n); thay the phan tu o i thanh gia tri n
  bit.pref_sum(n); goi ra tong cong don den n
  bai nay ko the lam bai range minium vi no update tu 0..n
 */
+
 template <class T> class BIT {
   private:
     int size;
@@ -26,5 +28,20 @@ template <class T> class BIT {
         T total = 0;
         for (; ind > 0; ind -= ind & -ind) { total += bit[ind]; }
         return total;
+    }
+    //tim min p sao cho sum(p) >= sum neu ko co thi kq = n-1
+    int lower_bound(T sum) {
+        int highest_one_bit = 1;
+        while (highest_one_bit << 1 <= bit.size())
+            highest_one_bit <<= 1;
+        int pos = 0;
+        for (size_t blockSize = highest_one_bit; blockSize != 0; blockSize >>= 1) {
+            int p = pos + blockSize ;
+            if (p < bit.size() && bit[p] < sum) {
+                sum -= bit[p];
+                pos += blockSize;
+            }
+        }
+        return pos;
     }
 };
